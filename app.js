a9113cd21eac651e2e8c27d0e16ae9bda46ccd7c
Naomi Kapele <naomikapele@booknowzambia.com>
2024-11-24 07:25:55 +0200

    filter functionality logic completed

--- app.js
+++ app.js
@@ -35,16 +35,20 @@ list.addEventListener("click", (e) => {
 
 const filterTodos = (term) => {
  Array.from(list.children)
- .filter((todo) => {
-   return !todo.textContent.includes(term);
-  
- });
+ .filter((todo) =>  !todo.textContent.toLowerCase().includes(term))
+ .forEach((todo) => todo.classList.add('filtered'))
+
+
+ Array.from(list.children)
+ .filter((todo) =>  todo.textContent.toLowerCase().includes(term))
+ .forEach((todo) => todo.classList.remove('filtered'))
+ 
 };
 
 // console.log(filterTodos.);
 
 // keyup event
 search.addEventListener('keyup', () => {
-  const term = search.value.trim();
+  const term = search.value.trim().toLowerCase();
   filterTodos(term);
 });
